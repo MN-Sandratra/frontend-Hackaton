416baba678d9b1e7d7fe50f1bbc808d86fe29460
Harry Michael <ando.lalaina1103@gmail.com>
2022-05-10 11:18:46 +0300

    Add province component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule, FaIconLibrary  } from '@fortawesome/angular-fontawesome';
 import { fas }from '@fortawesome/free-solid-svg-icons';
 import { far }from '@fortawesome/free-regular-svg-icons';
+import { ProvinceComponent } from './components/province/province.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvinceComponent
   ],
   imports: [
     BrowserModule,
